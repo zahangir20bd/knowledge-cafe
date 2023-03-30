c35c7ca51908990517b0c015f38bcc6043da9d69
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-03-31 00:26:04 +0600

    Creact react project and install bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="fs-6">Hello from Header</h1>
+    </div>
+  );
+};
+
+export default Header;
