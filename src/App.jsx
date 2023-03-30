5c6aa924382f1dbb733e5bc4fc977f1ae46cc38a
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-03-31 01:34:27 +0600

    Header Section complete

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container mx-auto w-100">
       <Header></Header>
       <Home></Home>
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import "../../assets/images/logo.png";
 
 const Header = () => {
   return (
-    <div>
-      <h1 className="fs-6">Hello from Header</h1>
+    <div className="bg-secondary-subtle mx-auto">
+      <nav className="navbar">
+        <div className="d-flex w-100 justify-content-between align-items-center">
+          <div>
+            <a className="navbar-brand" href="#">
+              <img src="logo.png" alt="" />
+            </a>
+          </div>
+          <div>
+            <a className="me-3" href="/home">
+              Home
+            </a>
+            <a className="me-3" href="/blogs">
+              Blogs
+            </a>
+            <a className="me-3" href="/about-us">
+              About Us
+            </a>
+            <img className="me-3 rounded-circle" src="user.jpg" alt="" />
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
