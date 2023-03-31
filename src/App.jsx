c1403443e8916eef8c5bd01739f811d576e39b6e
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-03-31 14:14:59 +0600

    calculation reading time and set to local Storage Done

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,13 @@ import Home from "./components/Home/Home";
 function App() {
   let readingTime = 0;
   const readTimeCalculation = (time) => {
+    const previousReadingTime = JSON.parse(localStorage.getItem("readTime"));
     readingTime = readingTime + time;
-    console.log(readingTime);
+    if (!previousReadingTime) {
+      localStorage.setItem("readTime", time);
+    } else {
+      localStorage.setItem("readTime", previousReadingTime + time);
+    }
   };
   return (
     <div className="App container mx-auto w-100">
